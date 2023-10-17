c332f87bd440920664874ee9b726d6ecbd7e7900
sarthik-7span <sarthik@7span.com>
2023-10-17 19:32:32 +0530

    react

--- src/component/Demo.jsx
+++ src/component/Demo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Text from "../component/default";
+import Text, { Text1, Text2, Text3, name } from "../component/default";
 function Demo() {
     return (
         <>
             <h1>Demo component</h1>
-            <h1>{Text}</h1>
+            <h1>{Text}//{Text1}//{Text2}</h1>
+            <p>{name()}</p>
         </>
     )
 }
--- src/component/default.jsx
+++ src/component/default.jsx
@@ -1,3 +1,11 @@
-const Text = "SARTHIK 122";
+const Text = "SARTHIK ";
+const Text1 = "SARTHIK 1";
+const Text2 = "SARTHIK 2";
+const Text3 = "SARTHIK 3";
+function name() {
+    let x = "Sarthik"
+    return x
+}
 
 export default Text;
+export { Text1, Text2, Text3, name }
